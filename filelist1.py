18c550e0ce5c15b4c16993a4d254de66a0379b81
Braeden42 <42975998+Braeden42@users.noreply.github.com>
2018-10-25 08:15:00 -0700

    Add files via upload

--- filelist1.py
+++ filelist1.py
@@ -1,10 +1,6 @@
 #lsit_example_0.py baf
-filelist1 = ["IMG_001.png","IMG_007.png","IMG_011.png","IMG_777.png"]
-print(filelist1)
-print(filelist2[3])
-print(filelist1[-3])
-print(filelist1[-1])
-***
+filelist1 = ["img "]
+
+
 thislist = ["apple", "banana", "cherry"]
-print(thislist[1]
-***
+print(thislist[1])
